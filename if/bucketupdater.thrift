33c2de79a99736ebb3e0b1d9e483ea04c6188cac
Alexey Makeyev <makeev@corp.mail.ru>
2013-09-11 15:49:00 +0400

    perl namespace for bucketupdater thrift entities

--- if/bucketupdater.thrift
+++ if/bucketupdater.thrift
@@ -19,6 +19,7 @@
 
 namespace cpp scribe.thrift
 namespace java com.facebook.infrastructure.service
+namespace perl Scribe.Thrift
 
 // BucketStoreMapping service exception
 exception BucketStoreMappingException {
