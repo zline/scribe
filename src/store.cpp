c286777f42238b177d26c887df9b04721e7c6ffa
Alexey Makeyev <makeev@corp.mail.ru>
2014-03-04 17:08:43 +0400

    rotation hour added to filename (as it should be)

--- src/store.cpp
+++ src/store.cpp
@@ -529,6 +529,9 @@ string FileStoreBase::makeBaseFilename(struct tm* creation_time) {
       filename << '-' << creation_time->tm_year + 1900  << '-'
 	       << setw(2) << setfill('0') << creation_time->tm_mon + 1 << '-'
 	       << setw(2) << setfill('0')  << creation_time->tm_mday;
+      
+      if (rollPeriod == ROLL_HOURLY)
+        filename << '-' << setw(2) << setfill('0') << creation_time->tm_hour;
     }
   } else {
     filename << baseFileName;
